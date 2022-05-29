cad4d457217c553de936f63f4fc200c00c2fdd0c
James Kalyan <contrib_x@protonmail.com>
2022-05-29 12:50:47 -0400

    Lazy defaults for polygon

--- src/polygon.lisp
+++ src/polygon.lisp
@@ -14,23 +14,25 @@
                :type (or integer float)
                :reader thickness)))
 
+(default-slot-value polygon %thickness 1.0)
+(default-slot-value polygon %rotation 0.0)
+(default-slot-value polygon %filled t)
+
 (defwriter-float radius polygon)
 (defwriter-float rot polygon %rotation)
 (defwriter-float thickness polygon)
 
 (definitializer polygon
-    (sides integer nil) (radius number float) (rot number float) (thickness number float 1.0))
+    (sides integer nil) (radius number float) (rot number float) (thickness number float))
 
 (defun make-polygon (x y sides radius color
-                     &key (filled t) (rotation 0) (thickness 1))
-  (make-instance 'polygon
-                 :pos (make-vector2 x y)
-                 :sides sides
-                 :radius radius
-                 :color color
-                 :filled filled
-                 :rot rotation
-                 :thickness thickness))
+                     &rest args &key rotation thickness filled)
+  (apply #'make-instance 'polygon
+         :pos (make-vector2 x y)
+         :sides sides
+         :radius radius
+         :color color
+         args))
 
 (defmethod draw-object ((obj polygon))
   (with-accessors ((center pos) (sides sides) (radius radius) (rot rot) (color color)
