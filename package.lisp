c5cfb553a8add3586d01f07c48199f953eddb997
James Kalyan <contrib_x@protonmail.com>
2022-06-01 21:17:22 -0400

    Export filled accessor

--- package.lisp
+++ package.lisp
@@ -95,7 +95,7 @@
    ;; Generic functions/methods
    :x :y :z :color :target :rot :zoom :x1 :y1 :x2 :y2 :width :height :len :offset :pos :draw-object
    :hit :low :high :r :g :b :a :dest :filter :origin :source :tint :radius :x3 :y3 :v1 :v2 :v3
-   :load-asset
+   :load-asset :filled
 
    ;; Math
    :vector2-subtract :vector2-add :vector2-scale :vector2-length
