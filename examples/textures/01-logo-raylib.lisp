360227488b665eb6b009de0d03c75062350bd6b7
Scott Helvick <4016577+shelvick@users.noreply.github.com>
2022-06-04 07:33:53 +0000

    Minor improvements to texture example 1

--- examples/textures/01-logo-raylib.lisp
+++ examples/textures/01-logo-raylib.lisp
@@ -7,10 +7,10 @@
 (defun main ()
   (with-window (:title "raylib [textures] example - texture loading and drawing")
     (let* ((image-size 256)
-           (scene (make-scene ((texass (make-instance 'texture-asset
-                                                      :path (asdf:system-relative-pathname
-                                                             :claylib
-                                                             "examples/textures/resources/raylib_logo.png"))))
+           (scene (make-scene ((texass (make-texture-asset 
+                                        (asdf:system-relative-pathname
+                                         :claylib
+                                         "examples/textures/resources/raylib_logo.png"))))
                               ((texture (make-texture texass
                                                       (/ (- (get-screen-width) image-size) 2.0)
                                                       (/ (- (get-screen-height) image-size) 2.0)))
@@ -19,6 +19,6 @@
                                                 :size 10
                                                 :color +gray+))))))
       (with-scene scene ()
-        (do-game-loop ()
+        (do-game-loop (:livesupport t)
           (with-drawing
             (draw-scene-all scene)))))))
