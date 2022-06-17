06f665fd00255f332ebb0f7075c1fad9ddd673ff
James Kalyan <contrib_x@protonmail.com>
2022-06-17 08:25:57 -0400

    Change core example 2 to use new scene workflow

--- examples/core/02-manager.lisp
+++ examples/core/02-manager.lisp
@@ -7,35 +7,41 @@
 (defun make-screen (color)
   (make-rectangle 0 0 *screen-width* *screen-height* color))
 
-(defparameter *logo* (make-scene () ((paint (make-screen *claylib-background*))
-                                     (text (make-text "LOGO SCREEN" 20 20
-                                                      :size 40 :color +lightgray+))
-                                     (subtext (make-text "WAIT for 2 SECONDS..." 290 220
-                                                         :size 20 :color +gray+)))))
+(defparameter *logo* (make-scene () () ((paint (make-screen *claylib-background*))
+                                        (text (make-text "LOGO SCREEN" 20 20
+                                                         :size 40 :color +lightgray+))
+                                        (subtext (make-text "WAIT for 2 SECONDS..." 290 220
+                                                            :size 20 :color +gray+)))))
 
-(defparameter *title* (make-scene () ((paint (make-screen +green+))
-                                      (text (make-text "TITLE SCREEN" 20 20
-                                                       :size 40 :color +darkgreen+))
-                                      (subtext (make-text "PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN"
-                                                          120 220
-                                                          :size 20 :color +darkgreen+)))))
+(defparameter *title* (make-scene (:free :later)
+                                  ()
+                                  ((paint (make-screen +green+))
+                                   (text (make-text "TITLE SCREEN" 20 20
+                                                    :size 40 :color +darkgreen+))
+                                   (subtext (make-text "PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN"
+                                                       120 220
+                                                       :size 20 :color +darkgreen+)))))
 
-(defparameter *gameplay* (make-scene () ((paint (make-screen +purple+))
-                                         (text (make-text "GAMEPLAY SCREEN" 20 20
-                                                          :size 40 :color +maroon+))
-                                         (subtext (make-text "PRESS ENTER or TAP to JUMP to ENDING SCREEN"
-                                                             130 220
-                                                             :size 20 :color +maroon+)))))
+(defparameter *gameplay* (make-scene (:free :later)
+                                     ()
+                                     ((paint (make-screen +purple+))
+                                      (text (make-text "GAMEPLAY SCREEN" 20 20
+                                                       :size 40 :color +maroon+))
+                                      (subtext (make-text "PRESS ENTER or TAP to JUMP to ENDING SCREEN"
+                                                          130 220
+                                                          :size 20 :color +maroon+)))))
 
-(defparameter *ending* (make-scene () ((paint (make-screen +blue+))
-                                       (text (make-text "ENDING SCREEN" 20 20
-                                                        :size 40 :color +darkblue+))
-                                       (subtext (make-text "PRESS ENTER or TAP to RETURN to TITLE SCREEN"
-                                                           120 220
-                                                           :size 20 :color +darkblue+)))))
+(defparameter *ending* (make-scene (:free :later)
+                                   ()
+                                   ((paint (make-screen +blue+))
+                                    (text (make-text "ENDING SCREEN" 20 20
+                                                     :size 40 :color +darkblue+))
+                                    (subtext (make-text "PRESS ENTER or TAP to RETURN to TITLE SCREEN"
+                                                        120 220
+                                                        :size 20 :color +darkblue+)))))
 
 (defun next-screen ()
-   "Is user indicating we can move to the next stage?"
+  "Is user indicating we can move to the next stage?"
   (or
    (is-key-pressed-p +key-enter+)
    (is-gesture-detected-p +gesture-tap+)))
@@ -43,16 +49,13 @@
 (defun main ()
   (with-window (:title "raylib [core] example - basic screen manager")
     (do-game-loop (:livesupport t
+                   :scene *logo*
                    :vars ((scenes `(,*logo* ,@(alexandria:circular-list *title* *gameplay* *ending*)))
                           (frame-count 0)))
       (incf frame-count)
       (when (or (and (next-screen) (not (eql (car scenes) *logo*)))
                 (and (eql (car scenes) *logo*) (> frame-count (* 2 *target-fps*))))
-        (setf scenes (cdr scenes)))
-      (let ((scene (car scenes)))
-        ;; TODO: Using WITH-SCENE within a game loop may be suboptimal for performance.
-        ;; It's not horrible; won't reload assets that are already loaded, for example.
-        ;; But still may cause issues with larger scenes. See #17.
-        (with-scene scene (:free :later)
-          (with-drawing (draw-scene-all scene)))))
+        (setf scenes (cdr scenes))
+        (switch-scene (car scenes)))
+      (with-drawing (draw-scene-all *scene*)))
     (mapcar #'unload-scene-all (list *logo* *title* *gameplay* *ending*))))
