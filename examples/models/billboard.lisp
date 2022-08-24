7256e43ee338ace87d4efc960dee9d51e8347670
James Kalyan <contrib_x@protonmail.com>
2022-08-24 14:41:02 -0400

    Update billboard example to use MAKE-SCENE-PRO

--- examples/models/billboard.lisp
+++ examples/models/billboard.lisp
@@ -9,26 +9,31 @@
                              "examples/models/resources/billboard.png"))))
 
 (defparameter *scene*
-  (make-scene ((bill-ass (car *assets*)))
-              ((grid (make-grid 10 1))
-               (bill (make-billboard bill-ass
-                                     *cam*
-                                     0 2 0
-                                     4 4
-                                     (make-rectangle 0 0
-                                                     (width bill-ass) (height bill-ass)
-                                                     +white+))))))
+  (make-scene-pro (:assets
+                   ((bill-ass (car *assets*)))
+                   :parameters
+                   ((cam (make-camera-3d 5 4 5
+                                         0 2 0
+                                         0 1 0
+                                         :mode +camera-orbital+)))
+                   :objects
+                   ((grid (make-grid 10 1))
+                    (bill (progn (print (claylib::c-struct cam))
+                                 (make-billboard bill-ass
+                                          cam
+                                          0 2 0
+                                          4 4
+                                          (make-simple-rec 0 0
+                                                           (width bill-ass) (height bill-ass)))))))))
 
 (defun main ()
   (with-window (:title "raylib [models] example - drawing billboards")
-    (let ((*cam* (make-camera-3d 5 4 5
-                                 0 2 0
-                                 0 1 0
-                                 :mode +camera-orbital+)))
-      (declare (special *cam*))
-      (with-scenes *scene*
+    (with-scenes *scene*
+      (with-scene-params (cam) *scene*
+        (print (claylib::c-struct cam))
         (do-game-loop (:livesupport t)
-          (update-camera *cam*)
+          (update-camera cam)
           (with-drawing ()
-            (with-3d-mode *cam* (draw-scene-all *scene*))
+            (with-3d-mode cam
+              (draw-scene-all *scene*))
             (draw-fps 10 10)))))))
