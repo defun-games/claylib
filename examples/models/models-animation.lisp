757b588329214e2584cfb47cd58b185c3a749b96
James Kalyan <contrib_x@protonmail.com>
2022-10-28 22:13:01 -0400

    Draw guy model with (manually) set materials

--- examples/models/models-animation.lisp
+++ examples/models/models-animation.lisp
@@ -21,12 +21,16 @@
                                        0 0 0
                                        0 1 0
                                        :mode +camera-free+))
-               
-               (model (claylib::make-model model-asset     ; TODO: This form causes a double free on quit
-                                  0 0 0
-                                  :rot-axis (make-vector3 1 0 0)
-                                  :rot-angle -90
-                                  :tint +white+))
+               (model (let ((m (claylib::make-model model-asset     ; TODO: This form causes a double free on quit
+                                                    0 0 0
+                                                    :rot-axis (make-vector3 1 0 0)
+                                                    :rot-angle -90
+                                                    :tint +white+)))
+                        (claylib/wrap:set-material-texture
+                         (autowrap:ptr (claylib::c-struct (elt (claylib::materials m) 0)))
+                         claylib/wrap:+material-map-diffuse+
+                         (claylib::c-asset model-texture))
+                        m))
                (grid (make-grid 10 1))
                (instructions (make-text "PRESS SPACE to PLAY MODEL ANIMATION"
                                         10 10
