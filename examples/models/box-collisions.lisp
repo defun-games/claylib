49340de81cd5176cae5bc6fb0a10ed5a77e2ed36
James Kalyan <contrib_x@protonmail.com>
2022-11-08 13:33:34 -0500

    Add box-collisions example. Needs PTs & maybe SPHERE support
    - Needs PTs for the bounding box collision functions
    - We may want to add a proper SPHERE class to Claylib.
    - We may want to integrate bounding box functionality into the classes
      which have Raylib bounding box support - but there is a clear
      argument for leaving that up to a user. An example of such an
      integration would be a class inheriting from CUBE that manages its
      own bounding box such that SETFing the position or size of the cube
      update its associated bounding box. Pass-throughs dealing with
      bounding boxes could be updated to also accept this cube+bbox
      class. Again, this could easily be left to a Claylib user.

---
+++ examples/models/box-collisions.lisp
@@ -0,0 +1,108 @@
+(in-package #:cl-user)
+(defpackage claylib/examples/box-collisions
+  (:use :cl :claylib)
+  (:export :main))
+(in-package #:claylib/examples/box-collisions)
+
+;; TODO sphere support in Claylib?
+(defclass sphere ()
+  ((%position :initarg :pos
+              :type rl-vector3
+              :accessor pos)
+   (%size :initarg :size
+          :type float
+          :accessor size)
+   (%color :initarg :color
+           :type rl-color
+           :accessor color)
+   (%filled :initarg :filled
+            :initform t
+            :type boolean
+            :accessor filled)))
+
+(defmethod draw-object ((obj sphere))
+  (if (filled obj)
+      (claylib/ll:draw-sphere (claylib::c-struct (pos obj))
+                              (size obj)
+                              (claylib::c-struct (color obj)))
+      (claylib/ll:draw-sphere-wires (claylib::c-struct (pos obj))
+                                    (size obj)
+                                    16 16 ; TODO don't hard-code
+                                    (claylib::c-struct (color obj)))))
+
+(defun cube-bbox (cube &key (bbox (make-instance 'rl-bounding-box
+                                                 :low (make-vector3 0 0 0)
+                                                 :high (make-vector3 0 0 0))))
+  "Return a bounding box matching a CUBE. Alloctaes a new RL-BOUNDING-BOX unless BBOX is given."
+  (setf (x (low bbox)) (- (x cube) (/ (width cube) 2))
+        (y (low bbox)) (- (y cube) (/ (height cube) 2))
+        (z (low bbox)) (- (z cube) (/ (len cube) 2))
+        (x (high bbox)) (+ (x cube) (/ (width cube) 2))
+        (y (high bbox)) (+ (y cube) (/ (height cube) 2))
+        (z (high bbox)) (+ (z cube) (/ (len cube) 2)))
+  bbox)
+
+(defparameter *scene*
+  (make-scene ()
+              ((camera (make-camera-3d 0 10 10
+                                       0 0 0
+                                       0 1 0))
+               (player (make-cube 0 1 2
+                                  1 2 1
+                                  +green+))
+               (pbbox (make-instance 'rl-bounding-box
+                                     :low (make-vector3 0 0 0)
+                                     :high (make-vector3 0 0 0)))
+               (enemy-box (make-cube -4 1 0
+                                     2 2 2
+                                     +gray+))
+               (box-wires (make-cube -4 1 0
+                                     2 2 2
+                                     +darkgray+
+                                     :filled nil))
+               (ebbox (make-instance 'rl-bounding-box
+                                     :low (make-vector3 0 0 0)
+                                     :high (make-vector3 0 0 0)))
+               (enemy-sphere (make-instance 'sphere
+                                            :pos (make-vector3 4 0 0)
+                                            :size 1.5
+                                            :color +gray+))
+               (sphere-wires (make-instance 'sphere
+                                            :pos (make-vector3 4 0 0)
+                                            :size 1.5
+                                            :color +darkgray+
+                                            :filled nil))
+               (grid (make-grid 10 1))
+               (text (make-text "Move player with cursors to collide"
+                                220 40 :size 20 :color +gray+)))))
+
+(defun main ()
+  (with-window (:title "raylib [models] example - box collisions")
+    (with-scenes *scene*
+      (with-scene-objects (camera player enemy-box enemy-sphere text pbbox ebbox box-wires sphere-wires) *scene*
+        (cube-bbox enemy-box :bbox ebbox)
+        (do-game-loop (:livesupport t
+                       :vars ((collision nil)))
+          ;; Move player
+          (cond ((is-key-down-p +key-right+) (incf (x player) 0.2))
+                ((is-key-down-p +key-left+)  (decf (x player) 0.2))
+                ((is-key-down-p +key-down+)  (incf (z player) 0.2))
+                ((is-key-down-p +key-up+)    (decf (z player) 0.2)))
+
+          ;; Collisions
+          (cube-bbox player :bbox pbbox)
+          (setf collision
+                (or ;; TODO pass-through
+                 (= 1 (claylib/ll:check-collision-boxes (claylib::c-struct pbbox)
+                                                        (claylib::c-struct ebbox)))
+                 ;; TODO pass-through
+                 (= 1 (claylib/ll:check-collision-box-sphere (claylib::c-struct pbbox)
+                                                             (claylib::c-struct (pos enemy-sphere))
+                                                             (size enemy-sphere)))))
+          (setf (color player) (if collision +red+ +green+))
+
+          (with-drawing ()
+            (with-3d-mode camera
+              (draw-objects player enemy-box enemy-sphere box-wires sphere-wires))
+            (draw-object text)
+            (draw-fps 10 10)))))))
