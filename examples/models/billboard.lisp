d52e60e5d23885a1b527ae0f333649d821ae9a1b
Scott Helvick <4016577+shelvick@users.noreply.github.com>
2022-11-30 23:50:07 +0000

    Make billboard example use MAKE-SCENE-PRO

--- examples/models/billboard.lisp
+++ examples/models/billboard.lisp
@@ -4,31 +4,28 @@
   (:export :main))
 (in-package #:claylib/examples/billboard)
 
-(defparameter *assets*
-  (list (make-texture-asset (claylib/examples:claylib-path
-                             "examples/models/resources/billboard.png"))))
-
 (defparameter *scene*
-  (make-scene ((bill-ass (car *assets*)))
-              ((grid (make-grid 10 1))
-               (bill (make-billboard bill-ass
-                                     *cam*
-                                     0 2 0
-                                     2 2
-                                     (make-rectangle 0 0
-                                                     (width bill-ass) (height bill-ass)
-                                                     +white+))))))
+  (make-scene-pro ((:params (cam (make-camera-3d 5 4 5
+                                                 0 2 0
+                                                 0 1 0
+                                                 :mode +camera-orbital+)))
+                   (:assets (bill-ass (make-texture-asset
+                                       (claylib/examples:claylib-path
+                                        "examples/models/resources/billboard.png"))))
+                   (:objects (grid (make-grid 10 1))
+                             (bill (make-billboard bill-ass
+                                                   cam
+                                                   0 2 0
+                                                   2 2
+                                                   (make-simple-rec 0 0
+                                                                    (width bill-ass) (height bill-ass))))))))
 
 (defun main ()
   (with-window (:title "raylib [models] example - drawing billboards")
-    (let ((*cam* (make-camera-3d 5 4 5
-                                 0 2 0
-                                 0 1 0
-                                 :mode +camera-orbital+)))
-      (declare (special *cam*))
-      (with-scenes *scene* ()
+    (with-scenes *scene* ()
+      (let ((cam (scene-param *scene* 'cam)))
         (do-game-loop (:livesupport t)
-          (update-camera *cam*)
+          (update-camera cam)
           (with-drawing ()
-            (with-3d-mode *cam* (draw-scene-all *scene*))
+            (with-3d-mode cam (draw-scene-all *scene*))
             (draw-fps 10 10)))))))
