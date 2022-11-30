4f8d227baa12d240bf9236ec905945474f43f81c
Scott Helvick <4016577+shelvick@users.noreply.github.com>
2022-11-30 23:48:08 +0000

    Taking a stab at MAKE-SCENE-PRO + improved docstrings

--- src/scene.lisp
+++ src/scene.lisp
@@ -2,7 +2,11 @@
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defclass game-scene ()
-    ((%game-objects :initarg :objects
+    ((%parameters :initarg :params
+                  :type hash-table
+                  :initform (make-hash-table :test #'equalp)
+                  :accessor params)
+     (%game-objects :initarg :objects
                     :type hash-table
                     :initform (make-hash-table :test #'equalp)
                     :accessor objects)
@@ -63,7 +67,14 @@ This is handy when the objects are in scope already, for example via WITH-SCENE-
 
 DEFER-INIT will defer initialization of the scene's OBJECTS until later (usually via WITH-SCENES or
 SET-UP-SCENE directly). This is useful when your scene contains objects like TEXTURES which require
-an OpenGL context before being loaded into the GPU."
+an OpenGL context before being loaded into the GPU.
+
+GC expresses a preference for whether garbage collection should be run when the scene closes.
+This can be overridden in WITH-SCENES.
+
+ASSETS and OBJECTS are lists of items of the form (BINDING VALUE). In a nutshell: assets are
+things that get loaded, and objects are things that get drawn. Values can reference previous
+bindings but will be initialized in the order they are declared."
   (let ((scene (gensym))
         (syms (when defer-init
                 (alexandria:make-gensym-list (length objects)))))
@@ -91,8 +102,65 @@ an OpenGL context before being loaded into the GPU."
                          collect `(setf (gethash ',binding (objects ,scene)) ,binding)))))
        ,scene)))
 
+(defmacro make-scene-pro ((&rest components) &key (gc t) (defer-init t))
+  "Make a GAME-SCENE.
+
+DEFER-INIT will defer initialization of the scene's OBJECTS until later (usually via WITH-SCENES or
+SET-UP-SCENE directly). This is useful when your scene contains objects like TEXTURES which require
+an OpenGL context before being loaded into the GPU.
+
+GC expresses a preference for whether garbage collection should be run when the scene closes.
+This can be overridden in WITH-SCENES.
+
+Each COMPONENT is a list whose head is either :PARAMS, :ASSETS, or :OBJECTS. In a nutshell: assets
+are things that get loaded, objects are things that get drawn, and parameters are generic bindings
+which are neither loaded nor drawn. Items after the head look like (BINDING VALUE). Values can
+reference previous bindings but will be initialized in the order they are declared."
+  (let* ((scene (gensym))
+         (items (reduce #'append
+                        (mapcar #'cdr components)))
+         (syms (when defer-init
+                 (alexandria:make-gensym-list (length items)))))
+    (flet ((group-case (group)
+             (ecase (car group)
+               (:params `(params ,scene))
+               (:assets `(assets ,scene))
+               (:objects `(objects ,scene)))))
+      `(let ((,scene (make-instance 'game-scene :gc ,gc))
+             ,@syms)
+         (declare (ignorable ,@syms))
+         ,(if defer-init
+              `(symbol-macrolet (,@(loop for sym in syms
+                                         for item in items
+                                         collect `(,(car item) (eager-future2:touch ,sym))))
+                 (let* (,@(loop for sym in syms
+                                for item in items
+                                collect `(,sym (eager-future2:pcall
+                                                (lambda () ,(cadr item))
+                                                :lazy))))
+                   ,@(loop for group in components
+                           with i = -1
+                           append (mapcar #'(lambda (item)
+                                              (incf i)
+                                              `(setf (gethash ',(car item) ,(group-case group))
+                                                     ,(elt syms i)))
+                                          (cdr group)))))
+              `(symbol-macrolet (,@items)
+                 ,@(loop for group in components
+                         append (mapcar #'(lambda (item)
+                                            `(setf (gethash ',(car item) ,(group-case group))
+                                                   ,(car item)))
+                                        (cdr group)))))
+         ,scene))))
+
 (defun scene-object (scene object)
   (gethash object (objects scene)))
+
+(defun scene-asset (scene asset)
+  (gethash asset (assets scene)))
+
+(defun scene-param (scene param)
+  (gethash param (params scene)))
 
 (defmacro with-scene-objects (objects scene &body body)
   `(symbol-macrolet ,(loop for obj in objects
@@ -116,12 +184,16 @@ Note: additional scenes can be loaded/GC'd at any point using {SET-UP,TEAR-DOWN}
         (t nil))))
 
 (defmethod set-up-scene ((scene game-scene))
-  (load-scene-all scene)
-  (maphash (lambda (binding val)
-             "Yield the futures in the objects hash table in place."
-             (when (typep val 'eager-future2:future)
-               (setf (gethash binding (objects scene)) (eager-future2:yield val))))
-           (objects scene)))
+  (flet ((yield-things (ht)
+           (maphash (lambda (binding val)
+                      "Yield the futures in the hash table in place."
+                      (when (typep val 'eager-future2:future)
+                        (setf (gethash binding ht) (eager-future2:yield val))))
+                    ht)))
+    (yield-things (assets scene))
+    (load-scene-all scene)
+    (yield-things (params scene))
+    (yield-things (objects scene))))
 
 (defmethod set-up-scene ((scene null)) ())
 
